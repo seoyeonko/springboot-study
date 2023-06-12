60d84a3173c98e245fb2508e9d4f094a038c4c0f
seoyeonko <seoyeon99o@naver.com>
2023-06-12 23:52:44 +0900

    study: jpa

--- src/main/java/com/example/sean/Member.java
+++ src/main/java/com/example/sean/Member.java
@@ -6,16 +6,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access= AccessLevel.PROTECTED)
+@NoArgsConstructor(access= AccessLevel.PROTECTED) // 2. 기본 생성자
 @AllArgsConstructor
 @Getter
-@Entity
+@Entity // 1. entity 지정
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id // 3. id 필드 기본키 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 4. 기본키 자동으로 1씩 증가
     @Column(name="id", updatable=false)
     private Long id; // db table id column 과 매칭
 
-    @Column(name="name", nullable=false)
+    @Column(name="name", nullable=false) // 5. name 이라는 not null 컬럼과 매핑
     private String name; // db table name column 과 매칭
 }
---
+++ src/main/java/com/example/sean/MemberService.java
@@ -0,0 +1,26 @@
+package com.example.sean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    public void test() {
+        // 1. 생성
+        memberRepository.save(new Member(1L, "A"));
+
+        // 2. 조쇠
+        Optional<Member> member = memberRepository.findById(1L); // 단건 조회
+        List<Member> allMembers = memberRepository.findAll(); // 전체 조회
+
+        // 3. 삭제
+        memberRepository.deleteById(1L);
+    }
+}
